e135d33afe3b0bd528cb4c3aab3ed1d5b0ac48fe
dileeparanawake <dileeparanawake@icloud.com>
2024-12-24 10:21:34 +0000

    add search-bar.css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <h1>Jammin for Spotify</h1>
+        <h1>Jammming for Spotify</h1>
         <p>
           Search your Spotify library, create a custom playlist, then save it to
           your account.
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,17 @@
 import React from "react";
+import "./search-bar.css";
 
-function SearchBar(props) {
+function SearchBar({ handleSubmit, searchTerm, handleChange }) {
   return (
-    <form
-      id="searchBar"
-      onSubmit={props.handleSubmit}
-      aria-label="Search songs"
-    >
+    <form id="searchBar" onSubmit={handleSubmit} aria-label="Search songs">
       <label htmlFor="searchInput">Search by song title</label>
       <input
         id="searchInput"
         type="text"
         placeholder="Search song..."
         name="searchInput"
-        value={props.searchTerm}
-        onChange={props.handleChange}
+        value={searchTerm}
+        onChange={handleChange}
       />
       <button type="submit">Search</button>
     </form>
